63b5e6f5171f2ce82cde39e2963361734224da43
Maicon <maiconthales34@hotmail.com>
2025-09-08 09:52:24 -0300

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/project/fake_data/FakeDataApplication.java
@@ -0,0 +1,13 @@
+package com.project.fake_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/fake_data/FakeDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fake_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
